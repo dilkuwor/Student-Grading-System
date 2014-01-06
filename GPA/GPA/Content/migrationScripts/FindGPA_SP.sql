88cd7c58d87d8ffab9378d2ce6dc042f012311c4
MPanahandeh <mpg.pan@gmail.com>
2014-01-06 14:06:38 -0600

    Fixed Push of FindGPA SP

--- GPA/GPA/Content/migrationScripts/FindGPA_SP.sql
+++ GPA/GPA/Content/migrationScripts/FindGPA_SP.sql
@@ -6,19 +6,16 @@ DROP PROCEDURE FindGPA
  GO
 
 Create PROCEDURE [dbo].[FindGPA]
-
- @UserID int
-
+@UserID int
  AS
 BEGIN
 SELECT R.FName +' '+R.LName AS Name, R.RegistrationID AS StudentID,c.Id AS CourseID, C.CourseName,c.Credit, G.GradeScore, G.GradeNumber, C.Credit*G.GradeNumber AS StudentCourseGrade
 
-FROM Registrations R 
-JOIN CourseUsers CU ON R.RegistrationID = CU.Users_Id 
-JOIN Courses C ON CU.Courses_Id = C.Id 
-JOIN StudentGrades SG ON C.Id = SG.CourseId 
+FROM Registrations R
+JOIN CourseUsers CU ON R.RegistrationID = CU.Users_Id
+JOIN Courses C ON CU.Courses_Id = C.Id
+JOIN StudentGrades SG ON C.Id = SG.CourseId
 JOIN Grades G ON SG.GradeId = G.Id
-
 WHERE R.RegistrationID = @UserID;
 END
 
