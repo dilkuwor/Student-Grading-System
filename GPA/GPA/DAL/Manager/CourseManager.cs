b6c5f9d45caf6e7446b25d5c7f6c1566242baabc
MPanahandeh <mpg.pan@gmail.com>
2014-01-07 00:42:20 -0600

    Rectify Course Search

--- GPA/GPA/DAL/Manager/CourseManager.cs
+++ GPA/GPA/DAL/Manager/CourseManager.cs
@@ -53,12 +53,22 @@ namespace GPA.DAL.Manager
 
         public List<Cours> getCoursesByName(string searchString)
         {
-            List<Cours> courses = new List<Cours>();
-            using (var db = new GPAEntities())
-            {
-                courses = (List<Cours>)db.Courses.Where(c => c.CourseName.ToUpper().Contains(searchString.ToUpper()));
-            }
-            return courses;
+            IQueryable<Cours> list;
+
+
+            //List<Cours> mycourse = db.Courses.ToList();
+            //List<Cours> res = new List<Cours>();
+            //foreach (var item in mycourse)
+            //{
+            //    if (item.CourseName.ToUpper().Contains(searchString.ToUpper()))
+            //        res.Add( item );
+            //}
+            //return res;
+            var db = new GPAEntities();
+            list = (IQueryable<Cours>)db.Courses.Where(c => c.CourseName.ToUpper().Contains(searchString.ToUpper()));
+
+
+            return list.ToList();
         }
     }
 }
