954ae1e82f3f0c5df9941bbfbe87a37fc2f6cb8a
MPanahandeh <mpg.pan@gmail.com>
2014-01-11 01:03:57 -0600

    Download

---
+++ GPA/GPA/DAL/Extended/StaffSearh.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GPA.Models;
+
+/*
+ * Project Name: GPA  
+ * Date Started: 01/03/2014
+ * Description: Handles Staff business logic
+ * Module Name: Search Module
+ * Developer Name: Kengsreng Tang
+ * Version: 0.1
+ * Date Modified:
+ */
+
+namespace GPA.DAL.Extended
+{
+    public class StaffSearh : ISearch<string, IQueryable<UserDetail>>
+    {
+        private GPAEntities db = new GPAEntities();
+        public IQueryable<UserDetail> FindByName(string search)
+        {
+            var staffs = (from u in db.Users
+                          join ud in db.UserDetails on u.UserID equals ud.UserID
+                          where u.Role == "Staff"
+                          select ud);
+            if (!String.IsNullOrEmpty(search))
+            {
+                staffs = staffs.Where(s => s.FName.ToUpper().Contains(search.ToUpper())
+                    || s.LName.ToUpper().Contains(search.ToUpper()));
+            }
+            return staffs;
+        }
+    }
+}
